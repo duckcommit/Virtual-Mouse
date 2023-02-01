81eb4294b3e8220bdc494136e52cc2b6538e5af0
VYSHNAV AJITH <puthen1977@gmail.com>
2023-02-01 20:58:05 +0530

    New Lines

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -5,6 +5,8 @@ import pyautogui
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils=mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y=0
 while True:
     success, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -20,10 +22,18 @@ while True:
             for id, landmark in enumerate(landmarks):
                 x=int(landmark.x*frame_width)
                 y=int(landmark.y*frame_height)
-                print(x,y)
                 if id==8:
                     cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
-                    pyautogui.moveTo(x, y)
+                    index_x=screen_width/frame_width*x
+                    index_y=screen_height/frame_height*y
+                    pyautogui.moveTo(index_x, index_y)
+                if id==4:
+                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
+                    thumb_x=screen_width/frame_width*x
+                    thumb_y=screen_height/frame_height*y
+                    if abs(index_y-thumb_y) <20:
+                        pyautogui.click()
+                        pyautogui.sleep(1)
                     
 
     if success:
