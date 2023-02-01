2fe66a215cd22003ba93c834a3c2a9bad86aaf59
VYSHNAV AJITH <puthen1977@gmail.com>
2023-02-01 20:23:20 +0530

    New Lines

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -1,7 +1,13 @@
 import cv2
+import mediapipe as mp
 cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
 while True:
     success, frame = cap.read()
+    rgb_frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    print(hands)
     if success:
         cv2.imshow("Virtual Mouse", frame)
     
